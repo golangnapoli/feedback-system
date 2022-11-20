a1c243c515e4ecdf7dea7c17c83c14681080faa5
Simone Guarneri <simone.guarneri.s@gmail.com>
2022-11-20 17:37:33 +0100

    feat:add button style, add profle name

--- web/src/components/FeedbackRow.tsx
+++ web/src/components/FeedbackRow.tsx
@@ -9,8 +9,9 @@ function FeedbackRow(): JSX.Element{
     return(
         <div className='flex flex-1 lg:flex-row flex-col min-h-max m-10 bg-white bg-opacity-20 backdrop-blur-lg rounded drop-shadow-lg w-2/3'>
             
-            <div className='flex flex-1 items-center justify-center lg:items-start'>
-                <img src="https://picsum.photos/100/100" alt="" className='rounded-[100%] lg:m-5 -mt-5 w-[80px]' />
+            <div className='flex flex-col flex-1 lg:items-start items-center lg:justify-start justify-center '>
+                <img src="https://picsum.photos/100/100" alt="" className='rounded-[100%] lg:m-5 -mt-5 w-[80px] shadow-lg' />
+                <h1 className='w-font-medium text-sm text-yellow-50 text-center ml-5 '>Lorem Ipsum</h1>
             </div>
 
             <div className='flex-5 lg:flex-4 lg:pr-10 p-5'>
@@ -21,10 +22,12 @@ function FeedbackRow(): JSX.Element{
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, aliquam!
                 </p>
 
-                <div className='flex space-x-10 justify-end'>
-                    <button className='mt-3'><GiBreadSlice /></button>
-                    <button className='mt-3'><BiComment/></button>
-                    <button className='mt-3'><SiGithub/></button>
+                <div className='flex lg:justify-end justify-center'>
+                    <div className='space-x-10 shadow-lg p-2'>
+                        <button className='mt-3'><GiBreadSlice size={25} color={"#FEE8FC"}/></button>
+                        <button className='mt-3'><BiComment size={25} color={"#06B6D4"}/></button>
+                        <button className='mt-3'><SiGithub size={25} color={"#5653ED"}/></button>
+                    </div>
                 </div>
             </div>
         </div>

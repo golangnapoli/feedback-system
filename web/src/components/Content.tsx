294bd562772051ea7e1241d815448e0d03ead819
Simone Guarneri <47464913+SimoneGuarneri@users.noreply.github.com>
2022-11-20 16:56:00 +0100

    feat:add components page, add logo, tsconfig (#1)

---
+++ web/src/components/Content.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';  
+import FeedbackRow from './FeedbackRow';
+
+function Content(): JSX.Element{
+
+    const test : Array<string> = ['test', 'test', 'test', 'test'];
+
+    return(
+        <div className='top-0 flex-2 lg:flex-2 sm:flex-5 bg-trasparent flex lg:h-full h-[90%]'>
+            
+            <div className='flex flex-col'>
+
+                <div className='flex-1 sm:flex-1 items-center justify-center w-full'>
+                    <img src="./assets/logo.svg" alt="" className='w-[300px] m-auto mt-10' />
+                </div>
+
+                <div className='flex flex-4 items-center justify-start mt-10 flex-col overflow-scroll'>
+                    <FeedbackRow/>
+                    <FeedbackRow/>
+                    <FeedbackRow/>
+                    <FeedbackRow/>
+                    <FeedbackRow/>
+                    <FeedbackRow/>
+                    <FeedbackRow/>
+                    <FeedbackRow/>
+                    <FeedbackRow/>
+                </div>
+            </div>
+
+
+        </div>
+
+    )
+
+}
+
+export default Content;
